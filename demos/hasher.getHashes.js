6152e9cb6e897c6cdb3395b25002f610087ce453
Ganesh K Bhat <ganeshsurfs@gmail.com>
2022-12-23 12:45:33 +0530

    [hasher-apis] 1. New features of hash-dehash file. 2. Adding hash/dehash file to different file destinations. 3. Adding verify hash using sha-hash of hashed file. 4. Considering complete encrypt-decrypt techniques with market encrypt-decrypt architectures. 5. Adding getciphers list and gethashes list functions

---
+++ demos/hasher.getHashes.js
@@ -0,0 +1,22 @@
+/**
+ * 
+ * Package: hasher-apis
+ * Author: Ganesh B
+ * Description: 
+ * Install: npm i hasher-apis --save
+ * Github: https://github.com/ganeshkbhat/apis-hasher
+ * npmjs Link: https://www.npmjs.com/package/hasher-apis
+ * File: demos/hasher.getHashes.js
+ * File Description: 
+ * 
+*/
+
+/* eslint no-console: 0 */
+
+'use strict';
+
+
+const path = require("path");
+const _filelock = require("../index.js");
+
+console.log(_filelock.getHashes());
