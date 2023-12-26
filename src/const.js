a6499458e06c7ce342d12ec0c36cd1f3fc44774b
Ganesh B <ganeshsurfs@gmail.com>
2023-12-26 12:47:47 +0530

    feat(feature): [COMMIT] Adding function to different files. Breacking changes in file naming. See if backward compatibility can be introduced.

--- src/const.js
+++ src/const.js
@@ -382,6 +382,58 @@ function getSymbols(symbolname) {
 }
 
 
+/**
+ * getCiphers
+ *
+ * @return {*[]} 
+ */
+function getCiphers() {
+    return require('crypto').getCiphers();
+}
+
+
+/**
+ * getHashes
+ *
+ * @return {*[]} 
+ */
+function getHashes() {
+    return require('crypto').getHashes();
+}
+
+
+/**
+ * getDiffieHellman
+ *
+ * @param {*} groupName
+ * @return {*[]} 
+ */
+function getDiffieHellman(groupName) {
+    return require('crypto').getDiffieHellman(groupName);
+}
+
+
+/**
+ * getFips
+ *
+ * @return {*[]} 
+ */
+function getFips() {
+    return require('crypto').getFips();
+}
+
+
+/**
+ * getRandomValues
+ *
+ * @param {*} typedArray
+ * @return {*[]} 
+ */
+function getRandomValues(typedArray) {
+    return require('crypto').getRandomValues(typedArray);
+}
+
+
 module.exports.getConstants = getConstants;
 module.exports.getSymbolsList = getSymbolsList;
 module.exports.getSymbols = getSymbols;
